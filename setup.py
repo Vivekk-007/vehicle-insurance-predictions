282b13cbdfdee1d13007ae3a70f8897b178ce7b1
vivekkk-007 <kumarvivek05093896@gmail.com>
2025-11-13 00:50:37 +0530

    project ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="vivek kumar",
+    author_email="sharmavivek3896@gmail.com",
+    packages=find_packages()
+)
