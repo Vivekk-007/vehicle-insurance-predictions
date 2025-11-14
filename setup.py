429e190207dc9803540ae982d3b492d60857136b
vivekkk-007 <kumarvivek05093896@gmail.com>
2025-11-14 22:28:25 +0530

    project structure ready

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="src",
     version="0.0.1",
     author="vivek kumar",
-    author_email="sharmavivek3896@gmail.com",
+    author_email="kumarvivek05093896@gmail.com",
     packages=find_packages()
 )
