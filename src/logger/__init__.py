b1ddfb9813ed408bd03339cd3e76ad923f3d5f8a
vivekkk-007 <kumarvivek05093896@gmail.com>
2026-02-03 23:51:02 +0530

    Add .project-root for deployment

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -11,7 +11,8 @@ MAX_LOG_SIZE = 5 * 1024 * 1024  # 5 MB
 BACKUP_COUNT = 3  # Number of backup log files to keep
 
 # Construct log file path
-log_dir_path = os.path.join(from_root(), LOG_DIR)
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+log_dir_path = os.path.join(PROJECT_ROOT, LOG_DIR)
 os.makedirs(log_dir_path, exist_ok=True)
 log_file_path = os.path.join(log_dir_path, LOG_FILE)
 
